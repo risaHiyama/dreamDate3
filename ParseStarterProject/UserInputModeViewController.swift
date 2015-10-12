017fdc39598ced6594f8b4055398d2f7d6ad7ab5
Risa Hiyama <risa.hiyama@gmail.com>
2015-10-13 00:28:13 +0900

    sorted out folder

--- ParseStarterProject/UserInputModeViewController.swift
+++ ParseStarterProject/UserInputModeViewController.swift
@@ -17,10 +17,9 @@ class UserInputModeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -37,5 +36,4 @@ class UserInputModeViewController: UIViewController {
             }
         }
     }
-
 }
